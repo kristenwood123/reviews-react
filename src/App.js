b9f9434e53f351e9b3d16fd544523615af658e41
Kristen <kristenwood123@gmail.com>
2021-02-20 12:45:02 -0800

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Review from './Review';
+import {FaGithubSquare} from 'react-icons/fa'
+
 function App() {
   return (
     <main>
-      <section className='container'>
-        <div className='title'>
+      <section className="container">
+        <div className="title">
           <h2>our reviews</h2>
-          <div className='underline'></div>
+          <div className="underline"></div>
         </div>
         <Review />
       </section>
